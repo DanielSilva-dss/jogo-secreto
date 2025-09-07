7c2cf81ffe75120f034282d6f3fab9db6a03303e
DanielSilva-dss <danielss.ds50@gmail.com>
2025-09-07 18:01:15 -0300

    teste

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js (manter no .gitignore)
+window.firebaseConfig = {
+    apiKey: "AIzaSyBBn-iq8g4EfbHUC9YyR9Qq8XVxVo_5KtM",
+    authDomain: "jogo-numero-secreto-ranking.firebaseapp.com",
+    projectId: "jogo-numero-secreto-ranking",
+    storageBucket: "jogo-numero-secreto-ranking.firebasestorage.app",
+    messagingSenderId: "484416436213",
+    appId: "1:484416436213:web:a1f9dbea9529f44cde12bc"
+};
