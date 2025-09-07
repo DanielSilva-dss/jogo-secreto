4b757abe076735b1195c9d47e937349739489e2e
DanielSilva-dss <danielss.ds50@gmail.com>
2025-09-07 19:41:05 -0300

    "remove firebase-config.js from repository"

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.local.js - apenas para desenvolvimento
-window.firebaseConfig = {
-    apiKey: "AIzaSyBBn-iq8g4EfbHUC9YyR9Qq8XVxVo_5KtM",
-    authDomain: "jogo-numero-secreto-ranking.firebaseapp.com",
-    projectId: "jogo-numero-secreto-ranking",
-    storageBucket: "jogo-numero-secreto-ranking.firebasestorage.app",
-    messagingSenderId: "484416436213",
-    appId: "1:484416436213:web:a1f9dbea9529f44cde12bc"
-};
