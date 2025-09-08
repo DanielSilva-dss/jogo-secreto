bcd92b5979164badd5543c41345631931b15c2ab
DanielSilva-dss <danielss.ds50@gmail.com>
2025-09-07 21:14:35 -0300

    add (vercel-build no projeto)

---
+++ vercel-build.js
@@ -0,0 +1,17 @@
+// vercel-build.js
+const fs = require("fs");
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const fileContent = `window.firebaseConfig = ${JSON.stringify(config, null, 2)};`;
+
+fs.writeFileSync("./public/firebase-config.js", fileContent);
+
+console.log("✅ firebase-config.js gerado com sucesso!");
